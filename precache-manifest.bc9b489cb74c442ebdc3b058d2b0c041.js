8b13886ec34c32f5a556cfa2d5733c99eb55342f
Lithuwix Toki <loremlisumdol@gmail.com>
2022-10-17 22:13:16 +0700

    Updates

---
+++ precache-manifest.bc9b489cb74c442ebdc3b058d2b0c041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93355d1714d9abdb2850fc53782ae4ff",
+    "url": "/ilink-test-app/index.html"
+  },
+  {
+    "revision": "0d5e2a61f99de51e9feb",
+    "url": "/ilink-test-app/static/css/main.cdd34d91.chunk.css"
+  },
+  {
+    "revision": "7137efe6e2cfedf638be",
+    "url": "/ilink-test-app/static/js/2.8080ae6e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ilink-test-app/static/js/2.8080ae6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5e2a61f99de51e9feb",
+    "url": "/ilink-test-app/static/js/main.111a0d29.chunk.js"
+  },
+  {
+    "revision": "932b08bbcbcf4bdc1372",
+    "url": "/ilink-test-app/static/js/runtime-main.6e57e3db.js"
+  }
+]);
